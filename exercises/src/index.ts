e0eab9fa1edc909faea3c240ced528549ed8227b
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-10-08 20:40:20 +0100

    Add GET /bmi endpoint

--- exercises/src/index.ts
+++ exercises/src/index.ts
@@ -1,9 +1,28 @@
 import express from "express";
+import { calculateBmi, parseValues } from "./bmiCalculator";
 
 const app = express();
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack!");
+});
+
+app.get("/bmi", (req, res) => {
+  try {
+    const query = req.query;
+
+    const { height, weight } = parseValues(query.height, query.weight);
+
+    const bmi = calculateBmi(height, weight);
+
+    res.send({
+      weight,
+      height,
+      bmi,
+    });
+  } catch (error) {
+    res.json({ error: "malformatted parameters" });
+  }
 });
 
 app.listen(3003, () => {
