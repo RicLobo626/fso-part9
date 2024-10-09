7919f0433cfdc4d60d6184897462ee075be94d83
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-10-10 00:36:56 +0100

    Add isGender validator

--- patientor-backend/src/utils/validators.ts
+++ patientor-backend/src/utils/validators.ts
@@ -1,9 +1,15 @@
+import { Gender } from "../types";
+
 export const isString = (text: unknown): text is string => {
   return typeof text === "string" || text instanceof String;
 };
 
 export const isDate = (date: unknown): date is string => {
   return isString(date) && Boolean(Date.parse(date));
+};
+
+export const isGender = (gender: unknown): gender is Gender => {
+  return isString(gender) && Object.values(Gender).map(String).includes(gender);
 };
 
 export const isObject = (obj: unknown): obj is object => {
