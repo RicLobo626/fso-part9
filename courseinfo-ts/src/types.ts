c1ed5bc1688c7a71c56df60605698e4bd5754ba3
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-10-10 20:09:15 +0100

    Add types for basic, group and background parts

--- courseinfo-ts/src/types.ts
+++ courseinfo-ts/src/types.ts
@@ -1,4 +1,28 @@
-export type CoursePart = {
+/** Bases **/
+
+interface CoursePartBase {
   name: string;
   exerciseCount: number;
-};
+}
+
+interface CoursePartDescriptionBase extends CoursePartBase {
+  description: string;
+}
+
+/** Extends **/
+
+interface CoursePartBasic extends CoursePartDescriptionBase {
+  kind: "basic";
+}
+
+interface CoursePartGroup extends CoursePartBase {
+  groupProjectCount: number;
+  kind: "group";
+}
+
+interface CoursePartBackground extends CoursePartDescriptionBase {
+  backgroundMaterial: string;
+  kind: "background";
+}
+
+export type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground;
