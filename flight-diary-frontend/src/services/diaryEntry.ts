2085eab2b94bfb089df5c2d6b36a8d360875ff54
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-10-11 00:16:30 +0100

    Add addDiaryEntry service

--- flight-diary-frontend/src/services/diaryEntry.ts
+++ flight-diary-frontend/src/services/diaryEntry.ts
@@ -1,10 +1,16 @@
 import axios from "axios";
-import { DiaryEntry } from "../types";
+import { DiaryEntry, NewDiaryEntry } from "../types";
 
 const baseUrl = "/api/diaries";
 
 export const getDiaryEntries = async () => {
   const { data } = await axios.get<DiaryEntry[]>(baseUrl);
+
+  return data;
+};
+
+export const addDiaryEntry = async (body: NewDiaryEntry) => {
+  const { data } = await axios.post<DiaryEntry>(baseUrl, body);
 
   return data;
 };
