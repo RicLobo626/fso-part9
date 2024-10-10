a71c3144c20cd5a6b124fcfaea0169b682514754
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-10-10 01:18:09 +0100

    Extend error handler to handle zod errors

--- patientor-backend/src/app.ts
+++ patientor-backend/src/app.ts
@@ -3,6 +3,7 @@ import "express-async-errors";
 import cors from "cors";
 import { diagnosesRouter, patientsRouter } from "./routes";
 import { ParsingError } from "./utils/parsers";
+import { z } from "zod";
 
 const app = express();
 
@@ -17,12 +18,16 @@ app.get("/api/ping", (_req, res) => {
 });
 
 const errorHandler: ErrorRequestHandler = (error: unknown, _req, res, next) => {
-  if (error instanceof ParsingError) {
-    res.status(400).json({ error: error.message });
-    return;
+  switch (true) {
+    case error instanceof z.ZodError:
+      res.status(400).json({ error: error.issues });
+      break;
+    case error instanceof ParsingError:
+      res.status(400).json({ error: error.message });
+      break;
+    default:
+      next(error);
   }
-
-  next(error);
 };
 
 app.use(errorHandler);
