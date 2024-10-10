430a59ec7cffeb95e4ad0f6d9af5bbe5d3d9dc1d
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-10-11 00:26:32 +0100

    Add form error message

--- flight-diary-frontend/src/App.tsx
+++ flight-diary-frontend/src/App.tsx
@@ -2,9 +2,11 @@ import { SyntheticEvent, useEffect, useState } from "react";
 import { addDiaryEntry, getDiaryEntries } from "./services/diaryEntry";
 import { DiaryEntry, NewDiaryEntry } from "./types";
 import { DiaryEntries, DiaryEntryForm } from "./components";
+import { isAxiosError } from "axios";
 
 function App() {
   const [diaryEntries, setDiaryEntries] = useState<DiaryEntry[]>([]);
+  const [formError, setFormError] = useState(null);
 
   useEffect(() => {
     const getAndSetDiaryEntries = async () => {
@@ -25,18 +27,26 @@ function App() {
 
     const body = Object.fromEntries(formData) as NewDiaryEntry;
 
-    const data = await addDiaryEntry(body);
+    try {
+      const data = await addDiaryEntry(body);
 
-    setDiaryEntries(diaryEntries.concat(data));
+      setDiaryEntries(diaryEntries.concat(data));
 
-    form.reset();
+      form.reset();
+    } catch (e) {
+      if (isAxiosError(e)) {
+        setFormError(e.response?.data);
+      } else {
+        console.error(e);
+      }
+    }
   };
 
   return (
     <>
       <h1>Flight Diary</h1>
 
-      <DiaryEntryForm onSubmit={handleSubmit} />
+      <DiaryEntryForm onSubmit={handleSubmit} error={formError} />
 
       <DiaryEntries diaryEntries={diaryEntries} />
     </>
--- flight-diary-frontend/src/components/DiaryEntryForm.tsx
+++ flight-diary-frontend/src/components/DiaryEntryForm.tsx
@@ -2,43 +2,46 @@ import { SyntheticEvent } from "react";
 
 type Props = {
   onSubmit: (event: SyntheticEvent<HTMLFormElement>) => void;
+  error: string | null;
 };
 
-export const DiaryEntryForm = ({ onSubmit }: Props) => {
+export const DiaryEntryForm = ({ onSubmit, error }: Props) => {
   return (
-    <form onSubmit={onSubmit}>
-      <div>
-        <label htmlFor="date">Date</label>
-        <input type="text" id="date" name="date" />
-      </div>
+    <section>
+      <h2>Add new entry</h2>
 
-      <div>
-        <label htmlFor="visibility">Visibility </label>
-        <select name="visibility" id="visibility">
-          <option value="great">Great</option>
-          <option value="good">Good</option>
-          <option value="ok">Ok</option>
-          <option value="poor">Poor</option>
-        </select>
-      </div>
+      <form onSubmit={onSubmit}>
+        <div style={{ color: "red", marginBottom: "1em" }}>{error}</div>
 
-      <div>
-        <label htmlFor="weather">Weather</label>
-        <select name="weather" id="weather">
-          <option value="sunny">Sunny</option>
-          <option value="rainy">Rainy</option>
-          <option value="cloudy">Cloudy</option>
-          <option value="stormy">Stormy</option>
-          <option value="windy">Windy</option>
-        </select>
-      </div>
-
-      <div>
-        <label htmlFor="comment">Comment</label>
-        <input type="text" id="comment" name="comment" />
-      </div>
-
-      <button type="submit">Add</button>
-    </form>
+        <div>
+          <label htmlFor="date">Date</label>
+          <input type="text" id="date" name="date" />
+        </div>
+        <div>
+          <label htmlFor="visibility">Visibility </label>
+          <select name="visibility" id="visibility">
+            <option value="great">Great</option>
+            <option value="good">Good</option>
+            <option value="ok">Ok</option>
+            <option value="poor">Poor</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="weather">Weather</label>
+          <select name="weather" id="weather">
+            <option value="sunny">Sunny</option>
+            <option value="rainy">Rainy</option>
+            <option value="cloudy">Cloudy</option>
+            <option value="stormy">Stormy</option>
+            <option value="windy">Windy</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="comment">Comment</label>
+          <input type="text" id="comment" name="comment" />
+        </div>
+        <button type="submit">Add</button>
+      </form>
+    </section>
   );
 };
