28d61361f5af360d5414dc4ebeeef1cfcbce7a41
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-10-11 20:33:17 +0100

    Update patients data + types

--- patientor-backend/src/types.ts
+++ patientor-backend/src/types.ts
@@ -1,11 +1,51 @@
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface Entry {}
-
 export type Diagnosis = {
   code: string;
   name: string;
   latin?: string;
 };
+
+type BaseEntry = {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis["code"]>;
+};
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+type HealthCheckEntry = BaseEntry & {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+};
+
+type HospitalEntry = BaseEntry & {
+  type: "Hospital";
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+};
+
+type OccupationalHealthcareEntry = BaseEntry & {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+};
+
+export type Entry = HospitalEntry | OccupationalHealthcareEntry | HealthCheckEntry;
+
+type UnionOmit<T, K extends string | number | symbol> = Omit<T, K>;
+
+export type EntryWithoutId = UnionOmit<Entry, "id">;
 
 export enum Gender {
   Male = "male",
@@ -23,6 +63,6 @@ export type Patient = {
   entries: Entry[];
 };
 
-export type NewPatient = Omit<Patient, "id">;
+export type NewPatient = Omit<Patient, "id" | "entries">;
 
 export type PublicPatient = Omit<Patient, "ssn">;
