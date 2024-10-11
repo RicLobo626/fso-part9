f2dd94a5d483f3dc6a1a5d4d6daa624e976856bc
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-10-11 22:31:07 +0100

    Add types for entries

--- patientor-frontend/src/types.ts
+++ patientor-frontend/src/types.ts
@@ -4,10 +4,49 @@ export interface Diagnosis {
   latin?: string;
 }
 
+type BaseEntry = {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis["code"]>;
+};
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+type HealthCheckEntry = BaseEntry & {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+};
+
+type HospitalEntry = BaseEntry & {
+  type: "Hospital";
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+};
+
+type OccupationalHealthcareEntry = BaseEntry & {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+};
+
+export type Entry = HospitalEntry | OccupationalHealthcareEntry | HealthCheckEntry;
+
 export enum Gender {
   Male = "male",
   Female = "female",
-  Other = "other"
+  Other = "other",
 }
 
 export interface Patient {
@@ -17,6 +56,7 @@ export interface Patient {
   gender: Gender;
   ssn?: string;
   dateOfBirth?: string;
+  entries: Entry[];
 }
 
 export type PatientFormValues = Omit<Patient, "id" | "entries">;
